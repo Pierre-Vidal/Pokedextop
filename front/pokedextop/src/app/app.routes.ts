8bfa2744e968f04443215ed7739b0c7026fb1f64
remi tardif <remitardif11@gmail.com>
2025-12-03 16:52:16 +0100

    feat -Ajout navBar et routes

--- front/pokedextop/src/app/app.routes.ts
+++ front/pokedextop/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login.js';
+import { Pokedex } from './pokedex/pokedex.js';
+import { Pokemon } from './pokemon/pokemon.js';
+import { ListPokemon } from './list-pokemon/list-pokemon.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: Login},
+    {path: 'pokedex', component: Pokedex},
+    {path: 'pokemon', component: Pokemon},
+    {path: 'list-pokemon', component: ListPokemon},
+
+    {path : '', redirectTo: 'pokemon', pathMatch: 'full'},
+];
--- front/pokedextop/src/app/login/login.ts
+++ front/pokedextop/src/app/login/login.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
