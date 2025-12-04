bcb77dc638cce69a86377aa3b541ea1a48d0ef3c
remi tardif <remitardif11@gmail.com>
2025-12-04 09:34:18 +0100

    Feat Page d'accueil

--- front/pokedextop/src/app/app.routes.ts
+++ front/pokedextop/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Login } from './login/login.js';
 import { Pokedex } from './pokedex/pokedex.js';
 import { Pokemon } from './pokemon/pokemon.js';
 import { ListPokemon } from './list-pokemon/list-pokemon.js';
+import { Home } from './home/home.js';
 
 export const routes: Routes = [
+    {path: 'home', component: Home},
     {path: 'login', component: Login},
     {path: 'pokedex', component: Pokedex},
     {path: 'pokemon', component: Pokemon},
     {path: 'list-pokemon', component: ListPokemon},
 
-    {path : '', redirectTo: 'pokemon', pathMatch: 'full'},
+    {path : '', redirectTo: 'home', pathMatch: 'full'},
 ];
