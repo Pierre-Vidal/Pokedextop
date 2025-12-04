652b84d48a8ef33dfb7b748e04c87716e2d4fd8a
ALLAOUI Ahmed <ahmed.allaoui@etu.unilim.fr>
2025-12-04 09:16:11 +0100

    AJout de l'affichage des pokemons

--- front/pokedextop/src/app/app.ts
+++ front/pokedextop/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListPokemon } from "./list-pokemon/list-pokemon";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListPokemon, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
